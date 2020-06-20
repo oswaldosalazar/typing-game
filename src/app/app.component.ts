c71b1e9ce4c7e8a7d8cd9cb3132817aeaedd3876
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-06-19 18:56:34 -0600

    Added basic template, bulma and faker sentence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
 }
